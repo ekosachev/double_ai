cf4bdfbced77b18ba545f9479ea7e1fb519efaf8
Vinermy <egorkosachev@vk.com>
2025-05-08 11:58:33 +0300

    feat: added route to get ordered messages

--- app/src/services/branch.py
+++ app/src/services/branch.py
@@ -1,9 +1,13 @@
+from typing import Optional
+
 from ..database import branch as db
 from ..database.models import models
-from ..database.general_db import check_if_set_of_objects_exist
+from ..database.general_db import check_if_object_exists, check_if_set_of_objects_exist
 from ..database.db import AsyncSession
 from ..schemas import branch as schemas
+from ..schemas import message as message_schemas
 from fastapi import HTTPException
+from ..database import message as message_db
 
 
 async def create_branch(
@@ -51,8 +55,87 @@ async def create_branch(
 
 
 async def get_branch_by_id(session: AsyncSession, id: int) -> schemas.Branch:
+    if not await check_if_object_exists(session, models.Branch, id):
+        raise HTTPException(404, f"Branch with {id=} does not exist")
     return await db.get_branch_by_id(session, id)
 
 
 async def get_branches(session: AsyncSession) -> list[schemas.Branch]:
     return await db.get_branches(session)
+
+
+async def get_root_message(
+    session: AsyncSession, id: int
+) -> Optional[message_schemas.Message]:
+    branch = await get_branch_by_id(session, id)
+    if branch.root_id is None:
+        return None
+
+    return await message_db.get_message_by_id(session, branch.root_id)
+
+
+async def get_head_message(
+    session: AsyncSession, id: int
+) -> Optional[message_schemas.Message]:
+    branch = await get_branch_by_id(session, id)
+    if branch.head_id is None:
+        return None
+
+    return await message_db.get_message_by_id(session, branch.head_id)
+
+
+async def update_root_id(
+    session: AsyncSession, branch_id: int, root_id: int
+) -> schemas.Branch:
+    if not await check_if_object_exists(session, models.Branch, branch_id):
+        raise HTTPException(404, f"Branch with {id=} does not exist")
+    if not await check_if_object_exists(session, models.Message, root_id):
+        raise HTTPException(404, f"Message with {id=} does not exist")
+
+    return await db.update_root_id(session, branch_id, root_id)
+
+
+async def update_head_id(
+    session: AsyncSession, branch_id: int, head_id: int
+) -> schemas.Branch:
+    if not await check_if_object_exists(session, models.Branch, branch_id):
+        raise HTTPException(404, f"Branch with {id=} does not exist")
+    if not await check_if_object_exists(session, models.Message, head_id):
+        raise HTTPException(404, f"Message with {id=} does not exist")
+
+    return await db.update_head_id(session, branch_id, head_id)
+
+
+async def get_all_messages(
+    session: AsyncSession, branch_id: int
+) -> list[message_schemas.Message]:
+    if not await check_if_object_exists(session, models.Branch, branch_id):
+        raise HTTPException(404, f"Branch with {id=} does not exist")
+
+    root_message = await get_root_message(session, branch_id)
+
+    if root_message is None:
+        raise HTTPException(
+            400, "Root message is None, cannot determine ordering of messages"
+        )
+    assert root_message is not None
+
+    messages = await db.get_all_messages(session, branch_id)
+    sorted_messages = [root_message]
+
+    while messages:
+        prev_ids = {msg.id for msg in sorted_messages}
+        to_remove: list[int] = []
+        for i, message in enumerate(messages):
+            if (
+                message.previous_message_id in prev_ids
+                or message.previous_message_id is None
+            ):
+                sorted_messages.append(message)
+                to_remove.append(i)
+
+        to_remove.reverse()
+        for i in to_remove:
+            messages.pop(i)
+
+    return sorted_messages
